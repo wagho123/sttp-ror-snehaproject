0aef1af4ad39dbfa35060012325fab43f916f9ab
snehal <sneha10waghmare@gmail.com>
2017-01-17 17:21:02 +0530

    my first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Myproject::Application.config.secret_token = '862e4090b75bc2fb8f03d5f7b31f1a93e76062d5b1ca33f55e6a5f1b254ea35f71c7879dbd0717a68ab6bf591151206a9f1c07c8a6fbd3f8b575288237a61021'
---
+++ db/migrate/20170117110124_create_authors.rb~
@@ -0,0 +1,11 @@
+class CreateAuthors < ActiveRecord::Migration
+  def change
+    create_table :authors do |t|
+      t.string :name
+      t.integer :age
+      t.string :address
+
+      t.timestamps
+    end
+  end
+end
